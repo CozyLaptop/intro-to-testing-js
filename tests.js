323eadbcba6ba3ac1c02b79d5ef3a5adfea27539
Albert Dominguez <ajd@Alberts-MacBook-Air.local>
2021-07-13 15:53:14 -0500

    add tests and ability to say hello to any input

--- tests.js
+++ tests.js
@@ -20,4 +20,13 @@ describe('sayHello', function() {
     it('should return a string when called', function() {
         expect(typeof sayHello()).toBe("string");
     });
+    it('should return "Hello, Jane!"', function() {
+        expect(sayHello()).toBe("Hello, Jane!");
+    });
+    it('should return "Hello, Alex!"', function() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it('should return "Hello, Pat!"', function() {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
 });
